8d2c169ae38d967891e9cf2bbceaeabd13d20af7
h10ytguf <h10ytguf@gmail.com>
2021-04-12 13:49:39 +0300

    tests folder with tests for tasks and users, refactoring index.js restructuring it across 2 files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const app = require('./app')
 
-const app = express()
 const PORT = process.env.PORT
-
-//tells express to parse the json data coming into an object and be accessable via req.body.
-app.use(express.json())
-//Includes the user router and uses its router requests.
-app.use(userRouter)
-//Includes the task router and uses its router requests.
-app.use(taskRouter)
-//-----------------------------------------
-
 
 //Listening to the PORT constant value, either predefined port or a environment port.
 app.listen(PORT, () => {
